bd6c429035a34d47d934403afb76fe5dd48ebe53
Nicolas Duminil <nicolas.duminil@simplex-software.fr>
2017-05-31 12:08:44 +0200

    Have replaced annotations by web.xml

--- src/main/java/fr/simplex_software/tests/App.java
+++ src/main/java/fr/simplex_software/tests/App.java
@@ -3,7 +3,7 @@ package fr.simplex_software.tests;
 import javax.ws.rs.*;
 import javax.ws.rs.core.*;
 
-@ApplicationPath("services")
-public class App extends Application
+//@ApplicationPath("services")
+public class App /*extends Application*/
 {
 }
--- src/main/java/fr/simplex_software/tests/TestServiceImpl.java
+++ src/main/java/fr/simplex_software/tests/TestServiceImpl.java
@@ -4,10 +4,18 @@ import java.util.*;
 
 import javax.ws.rs.core.*;
 
-public class TestServiceImpl implements TestService
+public class TestServiceImpl extends Application implements TestService
 {
   public Response testGetResource()
   {
     return Response.ok().build();
+  }
+
+  @Override
+  public Set<Class<?>> getClasses() 
+  {
+    Set<Class<?>> classes = new HashSet<Class<?>>();
+    classes.add(TestServiceImpl.class);
+    return classes;
   }
 }
